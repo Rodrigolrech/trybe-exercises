681a6d84bd007b6f8819b2e3aacd40b434b1ea57
Rodrigo Rech <rodrigorech92@gmail.com>
2021-02-24 18:41:40 -0300

    bloco 4 exercicio 1 criado e efetuado

---
+++ bloco_4/dia_1/exercicio1.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 7;
+
+let sum = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+let mod = a % b;
+
+console.log(sum);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(mod);
