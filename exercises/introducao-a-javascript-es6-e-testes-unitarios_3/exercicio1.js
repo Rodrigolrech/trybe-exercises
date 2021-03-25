d52d252c393eb813ec70ba7ac25697acc18312a8
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-25 18:14:24 -0300

    Exercicio 1 resolvido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio1.js
@@ -9,3 +9,12 @@ function sum(a, b) {
 }
 
 // implemente seus testes aqui
+
+assert.strictEqual(sum(4,5), 9, '4 + 5 = 9');
+assert.strictEqual(sum(0,0), 0);
+assert.throws(() => {
+  sum(0,'0'),
+  sum('0',0)
+})
+
+sum(4,'5');
