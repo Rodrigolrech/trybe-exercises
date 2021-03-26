c8ce75c679640e1468ed4170d46228ffab504aa0
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-26 07:35:23 -0300

    Exercicio 3 parte 2 Resolvido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-3.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-3.js
@@ -1,5 +1,13 @@
 const assert = require('assert');
 // escreva a função sumAllNumbers aqui
+const sumAllNumbers = (numbers) => {
+  let sum = 0;
+  for (const index in numbers) {
+    sum += numbers[index];
+  }
+  return sum;
+}
+
 
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
