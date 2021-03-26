676c50440310208ea810b10bb5e78bc4389cabd2
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-26 07:25:48 -0300

    Exercicio 1 parte 2 resolvido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-1.js
@@ -1,6 +1,14 @@
 const assert = require('assert');
 // escreva a função addOne aqui
 
+const addOne = (array) => {
+  copyArray =[];
+  for (const index in array) {
+    copyArray[index] = array[index] + 1;
+  }
+  return copyArray;
+}
+
 const myArray = [31, 57, 12, 5];
 const unchanged = [31, 57, 12, 5];
 const expected = [32, 58, 13, 6];
