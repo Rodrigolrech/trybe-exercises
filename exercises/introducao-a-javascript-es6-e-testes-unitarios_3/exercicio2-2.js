e985384edb46e3864ec9db0bd6e30fb03d3a1506
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-26 07:28:53 -0300

    Exercicio 2 parte 2 resolvido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-2.js
@@ -1,5 +1,12 @@
 const assert = require('assert');
 // escreva a função wordLengths aqui
+const wordLengths = (words) => {
+  const arrayWordLength = [];
+  for (const index in words) {
+    arrayWordLength[index] = words[index].length;
+  }
+  return arrayWordLength;
+}
 
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
