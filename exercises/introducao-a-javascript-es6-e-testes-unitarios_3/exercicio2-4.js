d817b8ad239220d1ab4a55cbef6fdc88c7342f4f
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-26 07:41:58 -0300

    Exercicio 4 parte 2 resolvido

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-4.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio2-4.js
@@ -1,6 +1,16 @@
 const assert = require('assert');
 // escreva a função findTheNeedle aqui
 
+const findTheNeedle = (array, word) => {
+  let result = -1;
+  for (const index in array) {
+    if (array[index] === word) {
+      result = parseInt(index);
+    }
+  }
+  return result;
+}
+
 let words = ['house', 'train', 'slide', 'needle', 'book'];
 let expected = 3;
 let output = findTheNeedle(words, 'needle');
